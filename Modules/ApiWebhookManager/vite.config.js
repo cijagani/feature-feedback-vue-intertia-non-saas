f57e8cc33fbed34d225cf55b71244c1703ed2c6c
chirag jagani <trueline.chirag@gmail.com>
2025-08-29 12:24:34 +0530

    cleaning-cj

--- Modules/ApiWebhookManager/vite.config.js
+++
@@ -1,9 +0,0 @@
-// ApiWebhookManager Module Vite Configuration
-// This file defines which CSS/JS files should be included for this module
-
-export const paths = [
-	'Modules/ApiWebhookManager/resources/assets/css/app.css',
-	'Modules/ApiWebhookManager/resources/assets/js/app.js',
-];
-
-export default { paths };
--- Modules/EmbeddedSignup/vite.config.js
+++
@@ -1,7 +0,0 @@
-
-export const paths = [
-    "Modules/EmbeddedSignup/resources/assets/css/app.css",
-    "Modules/EmbeddedSignup/resources/assets/js/app.js",
-];
-
-export default { paths };
